48c9d8542214f1ee288ab08635b5fa324912cdd6
NorthStarDigitalLLC <westyn@northstardigitalweb.com>
2026-03-10 16:33:37 -0500

    Create menu-editor.js

---
+++ assets/js/menu-editor.js
@@ -0,0 +1,4 @@
+// Full Menu Editor script from FaceBookMenuEditor.xml
+// Paste the entire script content here.
+// It starts with: (() => { const DEFAULT_IMAGE_URL = ...
+// and ends with the final closing })();
