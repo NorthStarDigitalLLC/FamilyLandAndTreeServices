a62680d1bb1aa5347673b56253b2ea197492596d
NorthStarDigitalLLC <westyn@northstardigitalweb.com>
2026-03-10 18:45:01 -0500

    Delete assets directory

--- assets/js/menu-editor.js
+++
@@ -1,4 +0,0 @@
-// Full Menu Editor script from FaceBookMenuEditor.xml
-// Paste the entire script content here.
-// It starts with: (() => { const DEFAULT_IMAGE_URL = ...
-// and ends with the final closing })();
